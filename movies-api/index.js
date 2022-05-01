652975fe9171ab2f04241dbb9ed21e753b9671d0
Simi <simi.popola@gmail.com>
2022-05-01 18:51:41 +0100

    Mongo lab user Favourites

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import './db';
 import './seedData'
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -10,12 +11,17 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.use('/api/movies', moviesRouter);
+
+app.use('/api/genres', moviesRouter);
+
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
 
-app.use(express.json());
 
-app.use('/api/genres', moviesRouter);
+
