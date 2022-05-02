9d4f40022cc13b4326dba044b83673944fa6bf8f
Simi <simi.popola@gmail.com>
2022-05-02 18:14:38 +0100

    Topic 10 - Passport

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,5 +1,5 @@
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
@@ -24,13 +24,9 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use(session({
-  secret: 'ilikecake',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(passport.initialize());
 
-app.use('/api/movies', authenticate, moviesRouter);
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 
 app.use('/api/movies', moviesRouter);
 
