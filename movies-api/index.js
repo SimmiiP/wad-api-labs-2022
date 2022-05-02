8f4d054ba1f5316b9675858f484123186a5e720b
Simi <simi.popola@gmail.com>
2022-05-02 17:14:46 +0100

    Mongo lab exercise done

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,7 +4,7 @@ import moviesRouter from './api/movies';
 import './db';
 import './seedData'
 import usersRouter from './api/users';
-
+import genresRouter from './api/genres';
 dotenv.config();
 
 const errHandler = (err, req, res, next) => {
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
 
-app.use('/api/genres', moviesRouter);
+app.use('/api/genres', genresRouter);
 
 app.use('/api/users', usersRouter);
 
