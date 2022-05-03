dc1770ec428010836fe8457ceb79ea2b83dd6592
Simi <simi.popola@gmail.com>
2022-05-03 09:51:02 +0100

    Topic 11 - Sign up

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
 import { PublicPage, Movies, Profile, HomePage } from "./pages";
 
+import LoginPage from "./loginPage";
+import AuthProvider from "./authContext";
+import AuthHeader from "./authHeader";
+import ProtectedRoutes from "./protectedRoutes";
+import SignUpPage from "./signUpPage";
+
 const App = () => {
   return (
     <BrowserRouter>
+      <AuthProvider>
+        <AuthHeader />
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -23,10 +31,17 @@ const App = () => {
         <Routes>
           <Route path="/public" element={ <PublicPage /> } />
           <Route path="/" element={ <HomePage /> } />
-          <Route path="/movies" element={ <Movies /> } />
-          <Route path="/profile" element={ <Profile /> } />
+          <Route path="/login" element={ <LoginPage /> } />
+
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
           <Route path="*" element={ <Navigate to="/" /> } />
+          <Route path="/signup" element={ <SignUpPage /> } />
         </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
